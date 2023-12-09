52ba5091dc9e683527e2225027e50b00f894b784
NadezhdaZakozhurnikova <nadya.zakozhurnikova@yandex.ru>
2023-12-09 21:01:10 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://d4ea6314-de67-4a21-92b8-3b51e1bb535a.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREAT_ORDERS = "/api/v1/orders"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Маша",
+    "lastName": "Иванова",
+    "address": "Крауля, 168В",
+    "metroStation": 11,
+    "phone": "+7 912 369 67 35",
+    "rentTime": 2,
+    "deliveryDate": "2023-12-09",
+    "comment": "Домофон не работает",
+    "color": [
+        "BLACK"
+    ]
+}
